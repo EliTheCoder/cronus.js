b8d9846501b8830ffbebebd696de3122035caac8
EliTheCoder <ex.games.org@gmail.com>
2020-09-24 10:52:47 -0500

    Taking dirname as arguement now

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ const fs = require("fs");
 const path = require("path");
 
 class Cronus {
-	constructor(file_path) {
+	constructor(dirname, file_path) {
 		if (!file_path) throw new Error("No file path was specified");
 		this.path = file_path;
-		if (!fs.existsSync(path.join(__dirname, file_path))) throw new Error("File does not exist");
-		this.data = JSON.parse(fs.readFileSync(path.join(__dirname, path)));
+		if (!fs.existsSync(path.join(dirname, file_path))) throw new Error("File does not exist");
+		this.data = JSON.parse(fs.readFileSync(path.join(dirname, path)));
 	}
 	save(data = null) {
 		if (data) this.data = data;
